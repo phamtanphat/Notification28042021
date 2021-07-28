b204c9cdc8f0cee81bd47f4f49eb54c3611ca362
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-28 20:58:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/notification28042021/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.notification28042021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mBtnNotification;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnNotification = findViewById(R.id.buttonNotification);
+    }
+}
