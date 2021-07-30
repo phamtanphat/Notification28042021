e2ecc45b4dab1d27436cda2feed0e0fe5739a0db
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-30 20:22:55 +0700

    priority

--- app/src/main/java/com/example/notification28042021/MainActivity.java
+++ app/src/main/java/com/example/notification28042021/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.notification28042021;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.view.View;
 import android.widget.Button;
 
@@ -31,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
                 builder.setShowWhen(true);
                 builder.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.hinhicon));
                 builder.setContentTitle("Thông báo!!");
+                builder.setPriority(Notification.PRIORITY_HIGH);
+                builder.setVibrate(new long[]{1000, 1000, 1000, 1000});
+                builder.setSound(Uri.parse("android.resource://" + getPackageName() +"/" + R.raw.tetdongday));
                 builder.setContentText("Ứng dụng có phiên bản mới");
 
                 NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
